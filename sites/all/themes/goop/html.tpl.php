8e4cc284c345e0f2a9c9124c3a6cadd7f7d07651
Claire Annan <claire.annan.me@gmail.com>
2016-01-08 21:54:37 -0500

    Update html.tpl.php.

--- sites/all/themes/goop/html.tpl.php
+++ sites/all/themes/goop/html.tpl.php
@@ -42,11 +42,18 @@
  *
  * @ingroup themeable
  */
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
-  "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
-
-<head profile="<?php print $grddl_profile; ?>">
+?>
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="<?php print $language->language; ?>"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="<?php print $language->language; ?>"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="<?php print $language->language; ?>"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html lang="<?php print $language->language; ?>">
+<!--<![endif]-->
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+  <meta name="robots" content="noindex, nofollow" />
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
