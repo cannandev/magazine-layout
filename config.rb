e98696e4ba4b012fe153ba57aa759d190bb86ebc
Claire Annan <claire.annan.me@gmail.com>
2016-01-10 01:02:49 -0500

    Added main js. Replace social links with fonticons. Fix main-menu for smaller screens.

--- config.rb
+++ config.rb
@@ -3,11 +3,11 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
-sass_dir = "scss"
-images_dir = "images"
-javascripts_dir = "js"
-fonts_dir = "fonts"
+css_dir = "/sites/all/themes/goop/css"
+sass_dir = "/sites/all/themes/goop/scss"
+images_dir = "/sites/all/themes/goop/images"
+javascripts_dir = "/sites/all/themes/goop/js"
+fonts_dir = "/sites/all/themes/goop/fonts"
 
 output_style = :nested
 
--- sites/all/themes/goop/js/main.js
+++ sites/all/themes/goop/js/main.js
@@ -1,8 +1,23 @@
-(function(){
-	
-	$(document).ready(function(){
-		$('#search-toggle').click(function(){
+(function($){
+
+	var toggleSearch = function() {
+		var searchToggle = $('.main-menu .last');
+		searchToggle.html('<i class="fa fa-search"></i>').attr('id', 'search-toggle');
+		searchToggle.click(function(){
 			$('#search').toggleClass('active');
 		});
+	};
+
+	var swapSocialLinks = function() {
+		$('.social-links li a[href*="facebook"]').html('<i class="fa fa-facebook"></i>');
+		$('.social-links li a[href*="twitter"]').html('<i class="fa fa-twitter"></i>');
+		$('.social-links li a[href*="instagram"]').html('<i class="fa fa-instagram"></i>');
+		$('.social-links li a[href*="tumblr"]').html('<i class="fa fa-tumblr"></i>');
+	};
+	
+	$(document).ready(function(){
+		toggleSearch();
+		swapSocialLinks();
 	});
-})();
+
+})(jQuery);
